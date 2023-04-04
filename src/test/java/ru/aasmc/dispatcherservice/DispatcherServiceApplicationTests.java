a3e904d42d78e7fb85ae351c715c8a5e476763f0
Alex Alexeyev <aasmc@yandex.ru>
2023-04-04 17:43:53 +0300

    Initial commit

---
+++ src/test/java/ru/aasmc/dispatcherservice/DispatcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aasmc.dispatcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
